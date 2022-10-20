72da33d5ab2393f769e775082a650e22eda3ceb4
yahel dooley <ydooley@gmail.com>
2022-10-19 17:15:15 -0700

    Completed all 3 Add methods with overloads

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(2, 4, true)); ;
         }
-    }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+		public static string Add(int a, int b, bool addDollar)
+		{
+            int sum = a + b;
+            if (addDollar)
+            {
+                string suffix = sum == 1 ? "dollar" : "dollars";
+                return $"{sum} {suffix}";
+            }
+            else
+            {
+                return $"{sum}";
+            }
+		}
+
+	}
 }
